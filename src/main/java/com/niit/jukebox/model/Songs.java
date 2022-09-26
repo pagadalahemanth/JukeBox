3ccfcd02c72586dceca0743ab0abe17404f65bde
pagadalahemanth07@gmail.com <pagadalahemanth07@gmail.com>
2022-09-26 11:22:44 +0530

    created model class

---
+++ src/main/java/com/niit/jukebox/model/Songs.java
@@ -0,0 +1,86 @@
+package com.niit.jukebox.model;
+
+public class Songs {
+    private int songId;
+    private String songName;
+    private String albumName;
+    private String artist;
+    private String genre;
+    private String duration;
+
+    public Songs() {
+    }
+
+    public Songs(String songName, String albumName, String artist, String genre, String duration) {
+        this.songName = songName;
+        this.albumName = albumName;
+        this.artist = artist;
+        this.genre = genre;
+        this.duration = duration;
+    }
+
+    public Songs(int songId, String songName, String albumName, String artist, String genre, String duration) {
+        this.songId = songId;
+        this.songName = songName;
+        this.albumName = albumName;
+        this.artist = artist;
+        this.genre = genre;
+        this.duration = duration;
+    }
+
+    public int getSongId() {
+        return songId;
+    }//setter method for songId is not necessary because in jukebox db we have set to auto_increment
+
+    public String getSongName() {
+        return songName;
+    }
+
+    public void setSongName(String songName) {
+        this.songName = songName;
+    }
+
+    public String getAlbumName() {
+        return albumName;
+    }
+
+    public void setAlbumName(String albumName) {
+        this.albumName = albumName;
+    }
+
+    public String getArtist() {
+        return artist;
+    }
+
+    public void setArtist(String artist) {
+        this.artist = artist;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public String getDuration() {
+        return duration;
+    }
+
+    public void setDuration(String duration) {
+        this.duration = duration;
+    }
+
+    @Override
+    public String toString() {
+        return "Songs{" +
+                "songId=" + songId +
+                ", songName='" + songName + '\'' +
+                ", albumName='" + albumName + '\'' +
+                ", artist='" + artist + '\'' +
+                ", genre='" + genre + '\'' +
+                ", duration='" + duration + '\'' +
+                '}';
+    }
+}
